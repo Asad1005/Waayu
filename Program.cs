6335f0f6c22c3182a4598599351b1e4b453ff802
Mohd Asad <93603390+Asad1005@users.noreply.github.com>
2024-07-30 15:40:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Interim.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<InterimContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("InterimContext") ?? throw new InvalidOperationException("Connection string 'InterimContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+
+//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+//{
+//    options.ExpireTimeSpan = TimeSpan.FromMinutes(60 * 1);
+//    options.LoginPath = "/Login/Create";
+//    options.AccessDeniedPath = "/Login/Create";
+
+
+//});
+
+//builder.Services.AddSession(options =>
+//{
+//    options.IdleTimeout = TimeSpan.FromMinutes(5);
+//    options.Cookie.HttpOnly = true;
+//    options.Cookie.IsEssential = true;
+//});
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+//app.UseSession();
+//app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
